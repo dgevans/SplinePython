4da40180719cc33d2495ce9e07186097d43a4c4f
David Evans <dgevans42@gmail.com>
2013-02-25 10:33:33 -0500

    Initial Commit

---
+++ SplinePython/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SplinePython
+//
+//  Created by David Evans on 2/25/13.
+//  Copyright (c) 2013 David Evans. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
